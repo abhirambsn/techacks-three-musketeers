237c48034c65e212868d5ecc82e95fb4c2c381d8
Ayush Bansal <ayushb6732@gmail.com>
2023-01-01 03:44:47 +0530

    Added custom css, created home and mission section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
-import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Mission from './components/Mission'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Navbar />
+      <Home />
+      <Mission />
     </div>
   )
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Navbar from './Navbar'
+import crowdfunding from '../crowdfunding.png'
+import { FaSpaceShuttle, FaTwitter, FaLinkedin } from 'react-icons/fa';
+
+const Home = () => {
+  return (
+    <section className='section home' id='home'>
+        <Navbar />
+        <div className='home-section'>
+            <div className='home-section-data'>
+              <div className='home-grid'>
+                <div className='grid-30'>
+                  <div className='outer-decor'>
+                    <div className='inner-decor-icon'><FaSpaceShuttle /></div>
+                    <div className='inner-decor-text'><span>Make it happen</span></div>
+                  </div>
+                </div>
+                <div className='grid-70'></div>
+              </div>
+              <div className='home-head'>
+                <p>Let's make the impossible, possible</p>
+              </div>
+              <div className='home-subhead'>
+                <p>Join us and be a part of something special. Together, we can make a difference and bring your dreams to reality.</p>
+              </div>
+              <div className='home-btn'>
+                <button className='btn-inverse'>Start Campaign</button>
+              </div>
+              <div className='home-socials'>
+                <span className='home-socials-text'>Available on </span> <span className='home-socials-socials'><FaTwitter /></span> <span className='home-socials-socials'><FaLinkedin /></span>
+              </div>
+            </div>
+            <div className='home-section-img'>
+                <img src={crowdfunding} alt="crowd" />
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Home
