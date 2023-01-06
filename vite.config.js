031e2dfe2fd4f8e9ccb55afd40c47ad7d119efb3
Abhiram B.S.N <abhirambsn@gmail.com>
2023-01-06 14:09:27 +0530

    RTM Build v6123 -> Production build fixed

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react()
+    react(),
+    splitVendorChunkPlugin()
   ],
-  define: {
-    'process.env': {}
+  
+  build: {
+    chunkSizeWarningLimit: 1000
   }
 })
