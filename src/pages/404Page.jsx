7993780f82052c6f1b5e42b5faee322efe219714
Abhiram B.S.N <abhirambsn@gmail.com>
2023-01-07 09:44:40 +0530

    RTM v2.0.1 -> Finished Styling

---
+++ src/pages/404Page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <div style={{height: '100vh', width: '100vw', display: 'flex'}}>
+
+    </div>
+  )
+}
+
+export default NotFoundPage
